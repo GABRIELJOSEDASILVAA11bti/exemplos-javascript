00d4a96dddeb1e45299f5dd2d208ca9c5491a01b
GABRIELJOSEDASILVAA11bti <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>
2025-05-04 16:24:54 -0300

    quarto commit sobre os exemplos de javascript

--- main.js
+++ main.js
@@ -26,3 +26,20 @@ let nome = prompt ("Digite o nome do funcionário:")
 
 alert (mensagemBoasVindas (nome, cargo))
 console.log (mensagemBoasVindas(nome, cargo))
+
+
+/* exemplo 4 */
+
+function multiplicacao (numero1, numero2) {
+return numero1 * numero2
+}
+
+let multiplicacao1 = multiplicacao (6, 7)
+let multiplicacao2 = multiplicacao (9, 10)
+let multiplicacao3 = multiplicacao (12, 25)
+
+console.log(multiplicacao1)
+console.log(multiplicacao2)
+console.log(multiplicacao3)
+
+
